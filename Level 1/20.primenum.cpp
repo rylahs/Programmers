00443c391ef76d7c631f40e5d2402382f63aa052
Chan ho Moon <minsonyo@naver.com>
2022-01-09 16:13:10 +0900

    PS Level 1 Solved
    PS Level 1 Solved

--- Level 1/20.primenum.cpp
+++ Level 1/20.primenum.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 int prime[1000002];
 
-int solution(int n) {
-	for (int i = 2; i <= n; i++)
+int solution(int n) { // 에라토스테네스의 체 이용
+	for (int i = 2; i <= n; i++) // 전역 배열에 각 항의 숫자 입력, 소수는 2부터 시작
 		prime[i] = i;
-	for (int i = 2; i <= n; i++) 
+	for (int i = 2; i <= n; i++)  
 	{
-		if (prime[i] == 0)
+		if (prime[i] == 0) // 0인 항목은 제외
 			continue;
-		for (int j = i + i; j <= n; j += i)
+		for (int j = i + i; j <= n; j += i) // i의 배수인 모든 배열 항목을 0으로 정리 
 			prime[j] = 0;
 	}
 	int answer = 0;
-	for (int i = 2; i <= n; i++)
+	for (int i = 2; i <= n; i++) // 0이 아닌 배열의 수를 count
 		if (prime[i] != 0)
 			answer++;
 	return answer;
