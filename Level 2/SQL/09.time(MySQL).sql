b1b7df01565d4d20bed87b784ad24af060443986
Chan ho Moon <minsonyo@naver.com>
2022-02-05 13:43:37 +0900

    Level 2 SQL Solved

--- Level 2/SQL/09.time(MySQL).sql
+++ Level 2/SQL/09.time(MySQL).sql
@@ -1,3 +1,8 @@
 -- Programmers Level 2. 입양 시각 구하기(1)(MySQL)
 -- https://programmers.co.kr/learn/courses/30/lessons/59412
 
+SELECT HOUR(DATETIME) as HOUR, COUNT(ANIMAL_ID) as COUNT 
+FROM ANIMAL_OUTS 
+GROUP BY HOUR 
+    HAVING HOUR BETWEEN 9 AND 19 
+ORDER BY HOUR
